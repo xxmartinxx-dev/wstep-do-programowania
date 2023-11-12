7ea1fba94a6b3be5f9c6c4551fc68acc28c5dfa9
xxmartinxx-dev <84991317+xxmartinxx-dev@users.noreply.github.com>
2023-11-12 12:17:33 +0100

    lab 0

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+# zad 1 sprawdzanie typów wyników działań
+print(type(1 + 2))
+print(type(1 + 4.5))
+print(type(3 / 2))
+print(type(4 / 2))
+print(type(3 // 2))
+print(type(-3 // 2))
+print(type(11 % 2))
+print(type(2 ** 10))
+print(type(8 ** (1/3)))
+
+#zad 2 wyjasnienie działań poszczególnych instrukcji
+# zmiana liczby w nawiasie na l. całkowitą
+print(int(3.0))
+# zmiania liczby w nawiasie na liczbę zmiennoprzecinkową
+print(float(3))
+# zmiana stringa w nawiasie na liczbę zmiennoprzecinkową
+print(float("3"))
+# zmiana liczby na tekst
+print(str(12.4))
+# Zmiana liczby na zmienną boolean - prawda/fałsz
+print(bool(0))
+
+#zad 3 skrypt pobiera długości boków i oblicza pole i obwód
 x = 5
 print("Obliczamy pole prostokąta:")
 
 y = int(input("Podaj 1 bok:"))
 x = int(input("Podaj 2 bok:"))
 pole = (y*x)
-print("Pole prostokąta =", pole)
+obwod = 2*x +2*y
+print("Pole prostokąta =", pole, "Obwód prostokąta =", obwod)
 
+#zad 4 skrypt pobiera od uzytkownika drogę oraz średnie spalanie (na 100km) i przewiduje zużycie paliwa
 print("Obliczamy zużycie paliwa - podaj dane")
 droga = float(input("Podaj droge:"))
 spalanie = float(input("Podaj śr. spalanie:"))
@@ -15,6 +41,8 @@ zuzycie = droga*spalanie/100
 koszt = zuzycie*cena
 print("Zużycie paliwa:",zuzycie, "Koszt:", koszt)
 
+
+#zad 4.1 skrypt losuje drogę oraz średnie spalanie (na 100km) i przewiduje zużycie paliwa
 print("Obliczamy zużycie paliwa - losowe dane")
 import random
 
@@ -25,3 +53,10 @@ print("Droga:", droga, "Spalanie:", spalanie)
 zuzycie = droga*spalanie/100
 koszt = zuzycie*cena
 print("Zużycie paliwa:", zuzycie, "Koszt:", koszt)
+
+#zad 5 skrypt wyliczający zużycie paliwa z modyfikacją na wykorzystanie f-string
+
+#ZADANIA DODATKOWE
+#zad 1 obliczamy równanie liniowe, współczynniki a i b podaje użytkownik
+#zad 2 Obliczamy pole trójkąta
+#zad 3 Kalkulator
